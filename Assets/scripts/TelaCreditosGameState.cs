4fa1feed3c51beae05fef18c43d5ac1d088fd5c5
Mauricio Braga <braga.mauricio@gmail.com>
2024-09-13 20:07:34 -0300

    adicionada telas gráficas no jogo.

--- Assets/scripts/TelaCreditosGameState.cs
+++ Assets/scripts/TelaCreditosGameState.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class TelaCreditosGameState : GameBaseState
 {
+    private GameObject telaCreditosJogo;
+
     public override void enterState(GameStateManager gameState)
     {
         Debug.Log("Entramos na tela de créditos.");
         gameState.mensagem.text = "tela créditos.";
+        // pega o game object com a tela de título, para poder ativar / desativar
+        // a sua aparição (via SpriteRenderer) ao entrar / sair estado.
+        telaCreditosJogo = GameObject.Find("tela_creditos_Treasure_Hunter_1280_1060");
+
+        // ativa o sprite render do gameObject da tela de título, exibindo-a na tela.
+        telaCreditosJogo.GetComponent<SpriteRenderer>().enabled = true;
+
     }
 
     public override void updateState(GameStateManager gameState)
@@ -21,5 +30,8 @@ public class TelaCreditosGameState : GameBaseState
     public override void leaveState(GameStateManager gameState)
     {
         Debug.Log("Saindo da tela de créditos.");
+        // desativa o sprite render do gameObject da tela de título, escondendo-a.
+        telaCreditosJogo.GetComponent<SpriteRenderer>().enabled = false;
+
     }
 }
--- Assets/scripts/TelaInicialGameState.cs
+++ Assets/scripts/TelaInicialGameState.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class TelaInicialGameState : GameBaseState
 {
+    private GameObject telaInicialJogo;
+
     public override void enterState(GameStateManager gameState)
     {
         Debug.Log("Entramos na Tela inicial.");
         gameState.mensagem.text = "tela inicial.";
+
+        // pega o game object com a tela de título, para poder ativar / desativar
+        // a sua aparição (via SpriteRenderer) ao entrar / sair estado.
+        telaInicialJogo = GameObject.Find("Tela_treasure_hunter_1280_x_1060");
+
+        // ativa o sprite render do gameObject da tela de título, exibindo-a na tela.
+        telaInicialJogo.GetComponent<SpriteRenderer>().enabled = true;
 
     }
 
@@ -22,5 +31,8 @@ public class TelaInicialGameState : GameBaseState
     public override void leaveState(GameStateManager gameState)
     {
         Debug.Log("Saindo da Tela inicial.");
+        // desativa o sprite render do gameObject da tela de título, escondendo-a.
+        telaInicialJogo.GetComponent<SpriteRenderer>().enabled = false;
+
     }
 }
